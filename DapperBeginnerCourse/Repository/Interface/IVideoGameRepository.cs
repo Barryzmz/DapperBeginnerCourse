2d67b298a9f17aaeac614cd73f388f3a1622247f
BarryZhuang <1007koi@gmail.com>
2025-08-19 00:26:29 +0800

    feat: 新增 Repository 層並安裝 Microsoft.Data.SqlClient

---
+++ DapperBeginnerCourse/Repository/Interface/IVideoGameRepository.cs
@@ -0,0 +1,10 @@
+﻿using DapperBeginnerCourse.Models;
+
+
+namespace DapperBeginnerCourse.Repository.Interface
+{
+    public interface IVideoGameRepository
+    {
+        Task<List<VideoGame>> GetAllAsync();
+    }
+}
---
+++ DapperBeginnerCourse/Repository/VideoGameRepository.cs
@@ -0,0 +1,41 @@
+﻿using Dapper;
+using DapperBeginnerCourse.Models;
+using DapperBeginnerCourse.Repository.Interface;
+using Microsoft.Data.SqlClient;
+using System.Data.SqlTypes;
+
+namespace DapperBeginnerCourse.Repository
+{
+    public class VideoGameRepository : IVideoGameRepository
+    {
+        private readonly IConfiguration _configuration;
+        public VideoGameRepository(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        private SqlConnection GetConnection()
+        {
+            return new SqlConnection(_configuration.GetConnectionString("DefaultConnection"));
+        }
+
+        public async Task<List<VideoGame>> GetAllAsync()
+        {
+            const string sql = @"
+                SELECT 
+                    Id,        
+                    Title,
+                    Developer,
+                    Platform,
+                    ReleaseDate
+                FROM VideoGames
+                ORDER BY Id";
+
+            using var connection = GetConnection();
+            await connection.OpenAsync();
+
+            var rows = await connection.QueryAsync<VideoGame>(sql);
+            return rows.ToList(); // 轉成 List<VideoGame>
+        }
+    }
+}
