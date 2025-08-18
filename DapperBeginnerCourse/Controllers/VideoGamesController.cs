0abb9f98f24af14130c2ec5eb5d5784edfba4e60
BarryZhuang <1007koi@gmail.com>
2025-08-19 00:29:50 +0800

    feat: 新增 VideoGamesController.cs

---
+++ DapperBeginnerCourse/Controllers/VideoGamesController.cs
@@ -0,0 +1,29 @@
+﻿using DapperBeginnerCourse.Models;
+using DapperBeginnerCourse.Repository.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DapperBeginnerCourse.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VideoGamesController : ControllerBase
+    {
+        private readonly IVideoGameRepository _videoGameRepository;
+
+        public VideoGamesController(IVideoGameRepository videoGameRepository)
+        {
+            _videoGameRepository = videoGameRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<VideoGame>>> GetAllAsync()
+        {
+            var videoGames = await _videoGameRepository.GetAllAsync();
+            return videoGames;
+        }
+
+    }
+
+
+}
