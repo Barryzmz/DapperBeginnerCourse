5894d8229b979a15cbf79837e4cbbd5fa279f3d8
BarryZhuang <1007koi@gmail.com>
2025-08-19 00:29:10 +0800

    feat: Program.cs 註冊 VideoGameRepository 至 DI 容器

--- DapperBeginnerCourse/Program.cs
+++ DapperBeginnerCourse/Program.cs
@@ -1,3 +1,7 @@
+using DapperBeginnerCourse.Repository.Interface;
+using DapperBeginnerCourse.Repository;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IVideoGameRepository, VideoGameRepository>();
 
 var app = builder.Build();
 
